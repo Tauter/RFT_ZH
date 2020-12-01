fb4ceee219f2bb7edbf832eac3912a0d5228e9ae
Tauter <harangi.ftam@gmail.com>
2020-12-01 11:04:47 +0100

    Első függvény létrehozása!

---
+++ RFT/src/main/java/Kod.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author haran
+ */
+public class Kod {
+   
+    public static int osszeAdas(int x, int y){
+        return x+y;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("5 + 5 eredménye: " + osszeAdas(5, 5));
+    }
+}
